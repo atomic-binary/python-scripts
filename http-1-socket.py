32b2212b7b13073ace00d93927f909713f06fab0
Abhijit Dange <81798406+atomic-binary@users.noreply.github.com>
2021-09-18 18:06:58 +0530

    Create http-1-socket.py

---
+++ http-1-socket.py
@@ -0,0 +1,7 @@
+# A simple script to create a socket that will connect to the URL provided, write to the socket and read responses from it.
+# Code is still in the works, figuring out how sockets work in a programme. will keep updating till it is complete.
+
+import socket
+import threading
+
+PORT = 5050
