a43f597058ddb9d83e35a7c619fff671b4172f47
Abhijit Dange <81798406+atomic-binary@users.noreply.github.com>
2021-10-07 13:44:32 -0400

    Update mac-changer-with-cmd-line-args.py

--- mac-changer-with-cmd-line-args.py
+++ mac-changer-with-cmd-line-args.py
@@ -3,10 +3,19 @@
 import subprocess
 import optparse
 
-# subprocess.call("ifconfig")
+subprocess.call("ifconfig")
 
 parser = optparse.OptionParser()
 parser.add_option("-i", "--interface", dest=interface, help="Interface of which mac address is to be changed.")
-parser.parse_args()
+parser.add_option("-m", "--mac", dest=new_mac_addr, help="New MAC address.")
 
-interface = input("Enter interface: ")
+(options, arguments) = parser.parse_args()
+
+interface = options.interface
+new_mac_addr = options.new_mac_addr
+
+print("[+] Changing MAC address for " + interface + " to " + new_mac_addr)
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac_addr])
+subprocess.call(["ifconfig", interface, "up"])
