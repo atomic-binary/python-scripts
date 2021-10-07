9703da83d891d583b53723d86c2ce1d7a37c5db6
Abhijit Dange <81798406+atomic-binary@users.noreply.github.com>
2021-10-07 11:29:25 -0400

    Update simple-mac-address-changer.py

--- simple-mac-address-changer.py
+++ simple-mac-address-changer.py
@@ -1,9 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import subprocess
 
+print("[+] Starting MAC address changer............\n")
 subprocess.call("ifconfig", shell=True)
 #Replace the interface name with eth0 in below line.
-subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw 00:11:22:33:44:55", shell=True)
-subprocess.call("ifconfig eth0 up", shell=True)
+# subprocess.call("ifconfig eth0 down", shell=True)
+# subprocess.call("ifconfig eth0 hw 00:11:22:33:44:55", shell=True)
+# subprocess.call("ifconfig eth0 up", shell=True)
+
+#Using variables for interface and mac address.
+interface = input("Enter the interface name: ")
+new_mac_addr = input("Please enter the new mac address: ")
+
+print("[+] Changing MAC address for interface: " + interface + " to: " + new_mac_addr)
+
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac_addr, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
